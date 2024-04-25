44ead6e571febb095af4ddc22ca364c905163b72
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-25 08:45:02 +0600

    commit -

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
     return (
         <div>
             <header className="hero h-[500px] mb-16" style={{ backgroundImage: `url(${coffee_banner})` }}>
-                <div className="hero h-[500px] bg-slate-950 bg-opacity-20"></div>
+                <div className="hero h-[500px] bg-slate-950 bg-opacity-10"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-3xl font-extrabold rancho-regular ">Would you like a Cup of Delicious Coffee?</h1>
+                        <h1 className="mb-5 text-3xl font-extrabold rancho-regular text-primary">Would you like a Cup of Delicious Coffee?</h1>
                         <p className="mb-5">It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
                         {/* <button className="btn btn-primary">Get Started</button> */}
                     </div>
